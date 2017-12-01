da730e29a35e1df3b1395af5e91a26bd7373a15d
Detik <the2ndflamingheart@gmail.com>
2017-12-01 11:24:20 +0700

    Remove fabric and belle

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import { Fabric } from 'office-ui-fabric-react/lib/Fabric';
-import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
 import logo from './logo.svg';
 import './App.css';
 
@@ -15,7 +13,6 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Fabric><DefaultButton>I am a button.</DefaultButton></Fabric>
       </div>
     );
   }
